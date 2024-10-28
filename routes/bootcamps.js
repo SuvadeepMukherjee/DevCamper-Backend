d21d346f436054235734e74965dc280eb82de591
Suvadeep <suvadeepm0709@gmail.com>
2024-10-29 00:16:16 +0530

    routes and controllers setup

---
+++ routes/bootcamps.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+const router = express.Router();
+
+router.route("/").get(getBootcamps).post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+router.get("/", (req, res) => {});
+router.get("/:id", (req, res) => {});
+router.post("/", (req, res) => {});
+router.put("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
